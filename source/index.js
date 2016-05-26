c4dbfebefb230c57103e3a709417ba3eae193b1f
Nathaniel Blackburn <support@nblackburn.uk>
2016-05-27 00:48:48 +0100

    Moved plugin to source folder

--- source/index.js
+++ source/index.js
@@ -1,52 +1,37 @@
-'use strict';
-
-const compiler = require('vueify').compiler;
+import {compiler} from 'vueify';
 
 /**
  * Vue Brunch
  * Adds support to Brunch for pre-compiling single file Vue components.
  * 
- * @version 1.0.0
+ * @version 1.1.0
  * @author Nathaniel Blackburn <support@nblackburn.uk> (http://nblackburn.uk)
  */
 
 class VueBrunch {
     
     constructor(config) {
-        
-        // Set the configuration.
         this.config = config && config.plugins && config.plugins.vue || {};
     }
     
     compile(file) {
         
-        // Check we have a configuration.
         if (this.config) {
-            
-            // Apply the configuration to the compiler.
             compiler.applyConfig(this.config);
         }
         
-        // Create a new promise.
         return new Promise((resolve, reject) => {
             
-            // Compile the component.
             compiler.compile(file.data, file.path, function (error, result) {
                 
-                // Reject if any errors were thrown.
                 if (error) {
-                    return reject(error);
+                    reject(error);
                 }
                 
-                // Resolve if we are good.
-                return resolve(result);
-                
+                resolve(result);
             });
-            
         });
-        
     }
-    
 }
 
 VueBrunch.prototype.extension = 'vue';
