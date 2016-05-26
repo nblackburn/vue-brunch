f745b8c5c10abb020eafc9f0554721ecbae5851b
Nathaniel Blackburn <support@nblackburn.uk>
2016-05-27 00:51:03 +0100

    Added tests

---
+++ test/plugin.js
@@ -0,0 +1,22 @@
+import test from 'ava';
+import VueBrunch from '../source';
+
+const instance = new VueBrunch;
+
+test('can compile', t => {
+
+    const file = {data: '<template><h1>Hello World!</h1></template>', path: 'views/test.vue'};
+
+    return instance.compile(file).then((result) => {
+        t.true(result.length > 0);
+    });
+});
+
+test('can compile statically', t => {
+
+    const file = {data: '<template><h1>Hello World!</h1></template>', path: 'views/test.vue'};
+
+    return instance.compileStatic(file).then((result) => {
+        t.true(result.length > 0);
+    });
+});
