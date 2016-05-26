7795e4156fd1171dbdf4d781c21684d0636bb2dd
Nathaniel Blackburn <support@nblackburn.uk>
2016-05-27 00:18:25 +0100

    Added tests

--- source/index.js
+++ source/index.js
@@ -1,56 +1,61 @@
-'use strict';
-
-const compiler = require('vueify').compiler;
+import {compiler} from 'vueify';
 
 /**
  * Vue Brunch
  * Adds support to Brunch for pre-compiling single file Vue components.
  * 
- * @version 1.0.0
+ * @version 1.1.0
  * @author Nathaniel Blackburn <support@nblackburn.uk> (http://nblackburn.uk)
  */
 
 class VueBrunch {
     
     constructor(config) {
-        
-        // Set the configuration.
         this.config = config && config.plugins && config.plugins.vue || {};
     }
     
     compile(file) {
         
-        // Check we have a configuration.
         if (this.config) {
-            
-            // Apply the configuration to the compiler.
             compiler.applyConfig(this.config);
         }
         
-        // Create a new promise.
         return new Promise((resolve, reject) => {
             
-            // Compile the component.
             compiler.compile(file.data, file.path, function (error, result) {
                 
-                // Reject if any errors were thrown.
                 if (error) {
-                    return reject(error);
+                    reject(error);
                 }
                 
-                // Resolve if we are good.
-                return resolve(result);
-                
+                resolve(result);
             });
-            
         });
-        
     }
-    
+
+    compileStatic(file) {
+
+        if (this.config) {
+            compiler.applyConfig(this.config);
+        }
+
+        return new Promise((resolve, reject) => {
+
+            compiler.compile(file.data, file.path, function (error, result) {
+
+                if (error) {
+                    reject(error);
+                }
+
+                resolve(result);
+            });
+        });
+    }
 }
 
 VueBrunch.prototype.extension = 'vue';
 VueBrunch.prototype.type = 'javascript';
 VueBrunch.prototype.brunchPlugin = true;
+VueBrunch.prototype.staticTargetExtension = 'js';
 
 module.exports = VueBrunch;
