327d2857fbbcd403d691de1d49b52f291ecbb1e2
Tom Quirk <tomquirk@Toms-MacBook-Pro.local>
2017-08-11 19:45:45 +1000

    initial cleanup

--- test.old.js
+++ test.old.js
@@ -5,7 +5,10 @@ const plugin = new VueBrunch;
 
 test('can compile', t => {
 
-    const file = {data: '<template><h1>Hello World!</h1></template><script>export default {name: \'helloWorld\'};</script>', path: 'views/test.vue'};
+    const file = {
+        data: '<template><h1>Hello World!</h1></template><script>export default {name: \'helloWorld\'};</script>',
+        path: 'views/test.vue'
+    };
 
     return plugin.compile(file).then((result) => {
         t.true(result.length > 0);
