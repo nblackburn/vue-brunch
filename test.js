6f610075aba3967a04a98a7f9b63788b1106e530
Tom Quirk <tomquirkacc@gmail.com>
2017-09-29 14:31:35 +1000

    revert test suite to ava; add editorconfig

--- test.js
+++ test.js
@@ -3,45 +3,39 @@
 
 'use strict';
 
-const expect = require('chai').expect;
+const test = require('ava').test;
 const VueBrunch = require('.');
 
-describe('vue-brunch', () => {
-  let plugin;
+test.beforeEach(t => {
+  t.context.plugin = new VueBrunch();
+});
 
-  beforeEach(() => {
-    plugin = new VueBrunch({
-      plugins: {},
-    });
+test('should be an object', t => {
+  t.true(t.context.plugin instanceof VueBrunch);
+});
+
+test('should have #compile method', t => {
+  t.true(t.context.plugin.compile && t.context.plugin.compile instanceof Function);
+});
+
+test('should compile a Vue template with no css', t => {
+  const file = {
+    data: '<template><h1>Hello World!</h1></template><script>export default {name: "hello"};</script>',
+    path: 'Test.vue',
+  };
+
+  return t.context.plugin.compile(file).then(result => {
+    t.true(result.length > 0);
   });
+});
 
-  it('should be an object', () => {
-    expect(plugin).to.be.an.instanceof(VueBrunch);
-  });
+test('should compile a Vue template with css', t => {
+  const file = {
+    data: '<style>h1 {color: red;}</style><template><h1>Hello World!</h1></template><script>export default {name: "hello"};</script>',
+    path: 'Test.vue',
+  };
 
-  it('should have #compile method', () => {
-    expect(plugin).to.respondTo('compile');
-  });
-
-  it('should compile a Vue template with no css', () => {
-    const file = {
-      data: '<template><h1>Hello World!</h1></template><script>export default {name: "hello"};</script>',
-      path: 'Test.vue',
-    };
-
-    return plugin.compile(file).then(result => {
-      expect(result.length > 0);
-    });
-  });
-
-  it('should compile a Vue template with css', () => {
-    const file = {
-      data: '<style>h1 {color: red;}</style><template><h1>Hello World!</h1></template><script>export default {name: "hello"};</script>',
-      path: 'Test.vue',
-    };
-
-    return plugin.compile(file).then(result => {
-      expect(result.length > 0);
-    });
+  return t.context.plugin.compile(file).then(result => {
+    t.true(result.length > 0);
   });
 });
